e385f99b3d5024dca1d75810976daacd5f326eda
johannpa <johann_paque@hotmail.fr>
2021-10-18 13:23:10 +0200

    React application to know the size of your screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  }
+  
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {windowWidth}
     </div>
   );
+  
 }
 
 export default App;
